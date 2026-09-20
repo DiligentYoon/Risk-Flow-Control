b9416b34caef616271fc6c0abb5c73cc9ccec76f
OKsusu1411 <hansusu1411@gmail.com>
2026-09-21 01:31:38 +0900

    migration

---
+++ src/tasks/02_safety_value/envs/R1/R1_fall/__init__.py
@@ -0,0 +1,25 @@
+import gymnasium as gym
+
+gym.register(
+    id="R1-fall", 
+    entry_point=f"{__name__}.R1_fall_env:R1FallEnv",
+    disable_env_checker=True,
+    kwargs={
+        # Environment-Specific Entry Point for Env Cfg Class
+        "env_cfg_entry_point": f"{__name__}.R1_fall_env_cfg:R1FallEnvCfg",
+        "rl_ppo_cfg_entry_point": f"{__name__}.cfg:ppo_cfg.yaml",
+        "rl_mappo_cfg_entry_point": f"{__name__}.cfg:mappo_cfg.yaml",
+    }
+)
+
+gym.register(
+    id="R1-fall-play", 
+    entry_point=f"{__name__}.R1_fall_env:R1FallEnv",
+    disable_env_checker=True,
+    kwargs={
+        # Environment-Specific Entry Point for Env Cfg Class
+        "env_cfg_entry_point": f"{__name__}.R1_fall_env_cfg:R1FallPlayEnvCfg",
+        "rl_ppo_cfg_entry_point": f"{__name__}.cfg:ppo_cfg.yaml",
+        "rl_mappo_cfg_entry_point": f"{__name__}.cfg:mappo_cfg.yaml",
+    }
+)
